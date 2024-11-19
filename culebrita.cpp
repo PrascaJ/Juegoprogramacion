e51bc749c2d7538386fa77648f23e73896f38c5e
PrascaJ <Juanppras@gmail.com>
2024-11-18 21:56:41 -0500

    Create culebrita.cpp

---
+++ culebrita.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+#include <conio.h>
+#include <studio.h>
+#include <time.h>
+
+#define ARRIBA 72
+#define IZQUIERDA 75
+#define ABAJO 80
+#define DERECHA 77
+#define ESC 27
+#define ENTER 13
+
+int cuerpo{200}[2];
+int 
